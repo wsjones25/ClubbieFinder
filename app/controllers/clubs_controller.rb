2c9e0a1266ea929fa176d8b368cfec5863d9d1ae
Will Jones <wsjones1101@gmail.com>
2018-11-11 13:26:18 +0800

    Added the club controller and integrated index action for club items

---
+++ app/controllers/clubs_controller.rb
@@ -0,0 +1,5 @@
+class ClubsController < ApplicationController
+	def index
+		@clubs = Club.all
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+  get 'clubs', to: 'clubs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

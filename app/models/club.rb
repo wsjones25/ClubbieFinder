3376283ae7bb6ae63d9c16a52ad7d5b64d959ae9
Will Jones <wsjones1101@gmail.com>
2018-11-12 23:00:06 +0800

    Implemented main_image default value for clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,9 +1,14 @@
 class Club < ApplicationRecord
-  	validates_presence_of :club_name, :sport, :address
+  	validates_presence_of :club_name, :sport, :address, :main_image, :active_status
 	enum active_status: { inactive: 0, active: 1}
 	extend FriendlyId
   	friendly_id :club_name, use: :slugged
   	has_many :teams
 
+  	after_initialize :set_defaults
 
+  	# Setting a default value of the main_image.
+  	def set_defaults
+  		self.main_image ||= "http://placehold.it/300x200"
+  	end
 end

b75a8689c5b6d6b0740e4228a4428beb8fff1595
Will Jones <wsjones1101@gmail.com>
2018-11-12 21:59:21 +0800

    Implemented data validations for clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,5 +1,7 @@
 class Club < ApplicationRecord
+  	validates_presence_of :club_name, :sport, :address
 	enum active_status: { inactive: 0, active: 1}
 	extend FriendlyId
   	friendly_id :club_name, use: :slugged
+
 end

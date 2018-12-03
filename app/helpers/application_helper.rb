d453623a79694effdd6f79f6be3849c77bdca1fc
Will Jones <wsjones1101@gmail.com>
2018-12-04 10:00:54 +1300

    Implemented view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def login_helper
+		if current_user
+	      link_to "Logout", destroy_user_session_path, method: :delete
+	    else
+	      (link_to "Sign up", new_user_registration_path) + 
+	      "<br>".html_safe + 
+	      (link_to "Login", user_session_path)
+	    end
+	end
 end

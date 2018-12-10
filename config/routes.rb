7a479729e8c485b17aa546ae1f2206540f398683
Will Jones <wsjones1101@gmail.com>
2018-12-11 11:10:16 +1300

    Changes made until bug found from Devise Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  devise_for :club_users, path: 'club_user'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
